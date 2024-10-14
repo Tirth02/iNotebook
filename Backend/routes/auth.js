413695fbe8128805a543b6b14885c9afa902e9c2
Tirth Patel <pateltirth5442@gmail.com>
2024-10-14 13:19:41 +0530

    Validation for signup added at backend

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -1,11 +1,24 @@
 const express = require('express');
+const { body, validationResult } = require('express-validator');
 const router = express.Router();
 const User = require("../models/User");
-router.post('/',(req,res) => {
-    console.log(req.body);
-    const user = User(req.body);
-    user.save();
-    res.send(req.body);
+router.post('/',[
+    body('name', 'Length of the name should be atleast 3 words').isLength({min:3}),
+    body('email','Enter a valid email').isEmail(),
+    body('password','Length of password must be more than 4 character').isLength({min : 5}),
+],(req,res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty())
+    {
+        return res.status(400).json({ errors: errors.array()});
+    }
+    User.create({
+        name: req.body.name,
+        password: req.body.password,
+        email: req.body.email,
+    }).then(user => res.json(user))
+    .catch(err => {console.log(err) 
+    res.json({error: 'User already registered with this email',message: err.message})});
 })
 
 module.exports = router
