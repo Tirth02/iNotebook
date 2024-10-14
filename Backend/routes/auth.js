0223638a62d472002b218fd1fe3c85fee093f04c
Tirth Patel <pateltirth5442@gmail.com>
2024-10-14 19:51:18 +0530

    change in method of response and port changed

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -1,24 +1,49 @@
-const express = require('express');
-const { body, validationResult } = require('express-validator');
+const express = require("express");
+const { body, validationResult } = require("express-validator");
 const router = express.Router();
 const User = require("../models/User");
-router.post('/',[
-    body('name', 'Length of the name should be atleast 3 words').isLength({min:3}),
-    body('email','Enter a valid email').isEmail(),
-    body('password','Length of password must be more than 4 character').isLength({min : 5}),
-],(req,res) => {
+
+// Create a User using: POST "/api/auth/createuser"/ No login required
+router.post(
+  "/createuser",
+  [
+    body("name", "Length of the name should be atleast 3 words").isLength({
+      min: 3,
+    }),
+    body("email", "Enter a valid email").isEmail(),
+    body(
+      "password",
+      "Length of password must be more than 4 character"
+    ).isLength({ min: 5 }),
+  ],
+  async (req, res) => {
+    // If there are errors, return Bad request and the errors
     const errors = validationResult(req);
-    if(!errors.isEmpty())
-    {
-        return res.status(400).json({ errors: errors.array()});
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
-    User.create({
+    try {
+      // check whether the user with this email exists already
+      let user = await User.findOne({ email: req.body.email });
+      if (user) {
+        return res
+          .status(400)
+          .json({ error: "Sorry user with this email already exists" });
+      }
+      // create a new user 
+      user = await User.create({
         name: req.body.name,
         password: req.body.password,
         email: req.body.email,
-    }).then(user => res.json(user))
-    .catch(err => {console.log(err) 
-    res.json({error: 'User already registered with this email',message: err.message})});
-})
+      });
+      res.json(user);
+    } 
+    // catch errors
+    catch (error) {
+      console.error(error.message);
+      res.status(500).send("Some error occured");
+    }
+  }
+);
 
-module.exports = router
+module.exports = router;
