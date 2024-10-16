7371f1a71dccbb1219823c60e99d20f2f49ccb6c
Tirth Patel <pateltirth5442@gmail.com>
2024-10-16 12:44:13 +0530

    Navbar modified and added basic form and card for notes

--- frontend/inotebook-frontend/src/App.js
+++ frontend/inotebook-frontend/src/App.js
@@ -1,9 +1,5 @@
 import "./App.css";
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import About from "./components/About";
@@ -14,12 +10,14 @@ function App() {
       <NoteState>
         <Router>
           <Navbar />
-          <Routes>
-            <Route exact path="/" element={<Home />} />
-            <Route exact path="/about" element={<About />} />
-          </Routes>
+          <div className="container">
+            <Routes>
+              <Route exact path="/" element={<Home />} />
+              <Route exact path="/about" element={<About />} />
+            </Routes>
+          </div>
         </Router>
-      </NoteState>  
+      </NoteState>
     </>
   );
 }
--- frontend/inotebook-frontend/src/context/notes/NoteState.js
+++ frontend/inotebook-frontend/src/context/notes/NoteState.js
@@ -1,24 +1,68 @@
 import noteContext from "./noteContext";
 import { useState } from "react";
-const NoteState = (props) =>{
-    const s1 = {
-        "name":"Tirth",
-        "class":"5b"
-    }
-    const [state,setState] = useState(s1);
-    const update = () =>{
-        setTimeout(() =>{
-            setState({
-                "name":"Harry",
-                "class":"10b"
-            })
-        },1000);
-    }
-    return(
-        <noteContext.Provider value={{state,update}}>
-            {props.children}
-        </noteContext.Provider>
-    )
-}
+const NoteState = (props) => {
+  const notesInitial = [
+    {
+      _id: "670e2378581e764365429525",
+      user: "670d3cda7b1b8d404cce0999",
+      title: "My First Note",
+      description: "This is my first note on cloud",
+      tag: "General",
+      date: "2024-10-15T08:10:32.908Z",
+      __v: 0,
+    },
+    {
+      _id: "670e2378581e764365429526",
+      user: "670d3cda7b1b8d404cce0999",
+      title: "My Second Note",
+      description: "This is my second note on cloud",
+      tag: "Youtube",
+      date: "2024-10-15T08:10:32.908Z",
+      __v: 0,
+    },
+    {
+      _id: "670e2378581e764365429526",
+      user: "670d3cda7b1b8d404cce0999",
+      title: "My Second Note",
+      description: "This is my second note on cloud",
+      tag: "Youtube",
+      date: "2024-10-15T08:10:32.908Z",
+      __v: 0,
+    },
+    {
+      _id: "670e2378581e764365429526",
+      user: "670d3cda7b1b8d404cce0999",
+      title: "My Second Note",
+      description: "This is my second note on cloud",
+      tag: "Youtube",
+      date: "2024-10-15T08:10:32.908Z",
+      __v: 0,
+    },
+    {
+      _id: "670e2378581e764365429526",
+      user: "670d3cda7b1b8d404cce0999",
+      title: "My Second Note",
+      description: "This is my second note on cloud",
+      tag: "Youtube",
+      date: "2024-10-15T08:10:32.908Z",
+      __v: 0,
+    },
+    {
+      _id: "670e2378581e764365429526",
+      user: "670d3cda7b1b8d404cce0999",
+      title: "My Second Note",
+      description: "This is my second note on cloud",
+      tag: "Youtube",
+      date: "2024-10-15T08:10:32.908Z",
+      __v: 0,
+    },
+  ];
+  const [notes, setNotes] = useState(notesInitial);
+  return (
+    <noteContext.Provider value={{ notes, setNotes }}>
+      {props.children}
+    </noteContext.Provider>
+  );
+};
 
 export default NoteState;
